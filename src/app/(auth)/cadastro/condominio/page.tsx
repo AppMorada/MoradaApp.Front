66234ed43cedc9b8039a284e06c65e3a9e84114e
mfjack <ferreira.marlon@live.com>
2024-02-28 22:43:31 -0300

    feat: atualização das páginas condominio e morador com components criado

--- src/app/(auth)/cadastro/condominio/page.tsx
+++ src/app/(auth)/cadastro/condominio/page.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import { useState } from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
 
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
@@ -10,145 +8,119 @@ import { FaEyeSlash } from 'react-icons/fa'
 import { IoEyeSharp } from 'react-icons/io5'
 import z from 'zod'
 
-import Input from '@/components/input'
+import { Button } from '@/components/ui/button'
+import Input from '@/components/ui/input'
+import { LinkLogin } from '@/components/ui/linkLogin'
+import { Logo } from '@/components/ui/logo'
+import PasswordInput from '@/components/ui/passwordInput'
 
 interface FormData {
-    fullName: string
-    cnpj: string
-    email: string
-    password: string
-    condominium: string
-    address: string
+   fullName: string
+   cnpj: string
+   email: string
+   password: string
+   condominium: string
+   address: string
 }
 
 const schema = z.object({
-    fullName: z.string().min(3, 'O nome deve ter no mínimo 3 caracteres'),
-    cnpj: z.string().length(14, 'O CNPJ deve conter 14 caracteres'),
-    email: z
-        .string()
-        .min(1, 'O e-mail é obrigatório')
-        .email('Formato de e-mail inválido'),
-    password: z
-        .string()
-        .min(8, 'A senha deve ter no mínimo 8 caracteres')
-        .regex(/[a-z]/, 'A senha deve conter pelo menos uma letra minúscula')
-        .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
-        .regex(
-            /[!@#$%^&*(),.?":{}|<>]/,
-            'A senha deve conter pelo menos um caractere especial'
-        ),
-    condominium: z.string().min(1, 'O nome do condomínio é obrigatório'),
-    address: z.string().min(1, 'O endereço é obrigatório')
+   fullName: z.string().min(3, 'O nome deve ter no mínimo 3 caracteres'),
+   cnpj: z.string().length(14, 'O CNPJ deve conter 14 caracteres'),
+   email: z
+      .string()
+      .min(1, 'O e-mail é obrigatório')
+      .email('Formato de e-mail inválido'),
+   password: z
+      .string()
+      .min(8, 'A senha deve ter no mínimo 8 caracteres')
+      .regex(/[a-z]/, 'A senha deve conter pelo menos uma letra minúscula')
+      .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
+      .regex(
+         /[!@#$%^&*(),.?":{}|<>]/,
+         'A senha deve conter pelo menos um caractere especial'
+      ),
+   condominium: z.string().min(1, 'O nome do condomínio é obrigatório'),
+   address: z.string().min(1, 'O endereço é obrigatório')
 })
 
 export default function CondominiumRegisterPage() {
-    const [showPassword, setShowPassword] = useState(false)
+   const [showPassword, setShowPassword] = useState(false)
 
-    const {
-        register,
-        handleSubmit,
-        reset,
-        formState: { errors }
-    } = useForm<FormData>({
-        resolver: zodResolver(schema)
-    })
+   const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors }
+   } = useForm<FormData>({
+      resolver: zodResolver(schema)
+   })
 
-    const onSubmit = (data: FormData) => {
-        reset()
-    }
+   const onSubmit = (data: FormData) => {
+      reset()
+   }
 
-    const passwordShow = () => {
-        setShowPassword(!showPassword)
-    }
+   const passwordShow = () => {
+      setShowPassword(!showPassword)
+   }
 
-    return (
-        <section className="flex min-h-screen flex-col items-center justify-center">
-            <Image
-                src="/assets/images/brand/logo.svg"
-                alt="Logo MoradaApp"
-                width={268}
-                height={98}
-            />
-            <form className="mt-12" onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col items-center justify-center gap-4">
-                    <Input
-                        label="Nome do usuário administrador"
-                        type="text"
-                        name="fullName"
-                        placeholder="Insira o nome do usuário"
-                        register={register}
-                        error={errors.fullName?.message}
-                    />
-                    <Input
-                        label="CNPJ"
-                        type="text"
-                        name="cnpj"
-                        placeholder="XXX.XXX.XXX/0001-XX"
-                        register={register}
-                        error={errors.cnpj?.message}
-                    />
-                    <Input
-                        label="E-mail"
-                        type="email"
-                        name="email"
-                        placeholder="Insira seu e-mail"
-                        register={register}
-                        error={errors.email?.message}
-                    />
-                    <div className="relative">
-                        <Input
-                            label="Senha"
-                            type={showPassword ? 'text' : 'password'}
-                            name="password"
-                            placeholder="Insira sua senha"
-                            register={register}
-                            error={errors.password?.message}
-                        />
-                        <div
-                            className="absolute right-5 top-6 cursor-pointer"
-                            onClick={passwordShow}
-                        >
-                            {showPassword ? (
-                                <FaEyeSlash size={20} />
-                            ) : (
-                                <IoEyeSharp size={20} />
-                            )}
-                        </div>
-                    </div>
-                    <Input
-                        label="Nome do Condomínio"
-                        type="text"
-                        name="condominium"
-                        placeholder="Insira o nome do condomínio"
-                        register={register}
-                        error={errors.condominium?.message}
-                    />
-                    <Input
-                        label="Endereço"
-                        type="text"
-                        name="address"
-                        placeholder="Insira o endereço do condomínio"
-                        register={register}
-                        error={errors.address?.message}
-                    />
-                </div>
-                <div className="mt-8 flex flex-col items-center justify-center">
-                    <button
-                        className="h-12 w-[442px] rounded-lg bg-primary font-extrabold text-background"
-                        type="submit"
-                    >
-                        Confirmar Cadastro
-                    </button>
-                    <div className="m-4 flex gap-1">
-                        <span className="font-medium">
-                            Já possui uma conta?
-                        </span>
-                        <Link className="font-medium text-primary" href="#">
-                            Faça o login
-                        </Link>
-                    </div>
-                </div>
-            </form>
-        </section>
-    )
+   return (
+      <section className="flex min-h-screen flex-col items-center justify-center">
+         <Logo />
+
+         <form className="mt-12" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col items-center justify-center gap-4">
+               <Input
+                  label="Nome do usuário administrador"
+                  type="text"
+                  name="fullName"
+                  placeholder="Insira o nome do usuário"
+                  register={register}
+                  error={errors.fullName?.message}
+               />
+               <Input
+                  label="CNPJ"
+                  type="text"
+                  name="cnpj"
+                  placeholder="XXX.XXX.XXX/0001-XX"
+                  register={register}
+                  error={errors.cnpj?.message}
+               />
+               <Input
+                  label="E-mail"
+                  type="email"
+                  name="email"
+                  placeholder="Insira seu e-mail"
+                  register={register}
+                  error={errors.email?.message}
+               />
+               <PasswordInput
+                  showPassword={showPassword}
+                  passwordShow={passwordShow}
+                  register={register}
+                  errors={errors}
+               />
+               <Input
+                  label="Nome do Condomínio"
+                  type="text"
+                  name="condominium"
+                  placeholder="Insira o nome do condomínio"
+                  register={register}
+                  error={errors.condominium?.message}
+               />
+               <Input
+                  label="Endereço"
+                  type="text"
+                  name="address"
+                  placeholder="Insira o endereço do condomínio"
+                  register={register}
+                  error={errors.address?.message}
+               />
+            </div>
+            <div className="mt-8 flex flex-col items-center justify-center">
+               <Button submit="Cadastrar" />
+               <LinkLogin text="Login" href="/login" />
+            </div>
+         </form>
+      </section>
+   )
 }
--- src/app/(auth)/cadastro/morador/page.tsx
+++ src/app/(auth)/cadastro/morador/page.tsx
@@ -1,134 +1,105 @@
 'use client'
 
-import Image from 'next/image'
-import Link from 'next/link'
-
-import { useForm } from 'react-hook-form'
-
-import Input from '@/components/input'
+import { useState } from 'react'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { z } from 'zod'
-import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 import { FaEyeSlash } from 'react-icons/fa'
 import { IoEyeSharp } from 'react-icons/io5'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import Input from '@/components/ui/input'
+import { LinkLogin } from '@/components/ui/linkLogin'
+import { Logo } from '@/components/ui/logo'
+import PasswordInput from '@/components/ui/passwordInput'
 
 interface FormData {
-    fullName: string
-    cpf: string
-    email: string
-    password: string
+   fullName: string
+   cpf: string
+   email: string
+   password: string
 }
 
 const schema = z.object({
-    fullName: z.string().min(3, 'O nome deve ter no mínimo 3 caracteres'),
-    cpf: z.string().length(11, 'O CPF deve conter 11 caracteres'),
-    email: z.string().min(1, 'O e-mail é obrigatório').email('Formato de e-mail inválido'),
-    password: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres')
-        .regex(/[a-z]/, 'A senha deve conter pelo menos uma letra minúscula')
-        .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
-        .regex(/[!@#$%^&*(),.?":{}|<>]/, 'A senha deve conter pelo menos um caractere especial')
-
+   fullName: z.string().min(3, 'O nome deve ter no mínimo 3 caracteres'),
+   cpf: z.string().length(11, 'O CPF deve conter 11 caracteres'),
+   email: z
+      .string()
+      .min(1, 'O e-mail é obrigatório')
+      .email('Formato de e-mail inválido'),
+   password: z
+      .string()
+      .min(8, 'A senha deve ter no mínimo 8 caracteres')
+      .regex(/[a-z]/, 'A senha deve conter pelo menos uma letra minúscula')
+      .regex(/[A-Z]/, 'A senha deve conter pelo menos uma letra maiúscula')
+      .regex(
+         /[!@#$%^&*(),.?":{}|<>]/,
+         'A senha deve conter pelo menos um caractere especial'
+      )
 })
 
 export default function ResidentRegisterPage() {
-    const [showPassword, setShowPassword] = useState(false)
+   const [showPassword, setShowPassword] = useState(false)
 
-    const { 
-        register, 
-        handleSubmit,
-        reset, 
-        formState: { errors } 
-    } = useForm<FormData>({
-        resolver: zodResolver(schema)
-    })
+   const {
+      register,
+      handleSubmit,
+      reset,
+      formState: { errors }
+   } = useForm<FormData>({
+      resolver: zodResolver(schema)
+   })
 
-    const onSubmit = (data: FormData) => {
-        reset()
-    }
+   const onSubmit = (data: FormData) => {
+      reset()
+   }
 
-    const passwordShow = () => {
-        setShowPassword(!showPassword)
-    }
+   const passwordShow = () => {
+      setShowPassword(!showPassword)
+   }
 
-    return (
-        <section className="flex min-h-screen flex-col items-center justify-center">
-            <Image
-                src="/assets/images/brand/logo.svg"
-                alt="Logo MoradaApp"
-                width={268}
-                height={98}
-            />
-
-            <form className="mt-12" onSubmit={handleSubmit(onSubmit)}>
-                <div className="flex flex-col items-center justify-center gap-4">
-                    <Input
-                        label=" Nome Completo"
-                        type="text"
-                        name="fullName"
-                        placeholder="Insira o nome do usuário"
-                        register={register}
-                        error={errors.fullName?.message}
-                        />
-
-                    <Input
-                        label=" CPF"
-                        type="text"
-                        name="cpf"
-                        placeholder="000.000.000-00"
-                        register={register}
-                        error={errors.cpf?.message}
-                        />
-
-                    <Input
-                        label="E-mail"
-                        type="email"
-                        name="email"
-                        placeholder="Insira seu e-mail"
-                        register={register}
-                        error={errors.email?.message}
-                    />
-
-<div className="relative">
-                        <Input
-                            label="Senha"
-                            type={showPassword ? 'text' : 'password'}
-                            name="password"
-                            placeholder="Insira sua senha"
-                            register={register}
-                            error={errors.password?.message}
-                        />
-                        <div
-                            className="absolute right-5 top-6 cursor-pointer"
-                            onClick={passwordShow}
-                        >
-                            {showPassword ? (
-                                <FaEyeSlash size={20} />
-                            ) : (
-                                <IoEyeSharp size={20} />
-                            )}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="mt-8 flex flex-col items-center justify-center">
-                    <button
-                        className="h-12 w-[442px] rounded-lg bg-primary font-extrabold text-background"
-                        type="submit"
-                    >
-                        Confirmar Cadastro
-                    </button>
-
-                    <div className="m-4 flex gap-1">
-                        <span className="font-medium">
-                            Já possui uma conta?
-                        </span>
-                        <Link className="font-medium text-primary" href="#">
-                            Faça o login
-                        </Link>
-                    </div>
-                </div>
-            </form>
-        </section>
-    )
+   return (
+      <section className="flex min-h-screen flex-col items-center justify-center">
+         <Logo />
+         <form className="mt-12" onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col items-center justify-center gap-4">
+               <Input
+                  label=" Nome Completo"
+                  type="text"
+                  name="fullName"
+                  placeholder="Insira o nome do usuário"
+                  register={register}
+                  error={errors.fullName?.message}
+               />
+               <Input
+                  label=" CPF"
+                  type="text"
+                  name="cpf"
+                  placeholder="000.000.000-00"
+                  register={register}
+                  error={errors.cpf?.message}
+               />
+               <Input
+                  label="E-mail"
+                  type="email"
+                  name="email"
+                  placeholder="Insira seu e-mail"
+                  register={register}
+                  error={errors.email?.message}
+               />
+               <PasswordInput
+                  showPassword={showPassword}
+                  passwordShow={passwordShow}
+                  register={register}
+                  errors={errors}
+               />
+            </div>
+            <div className="mt-8 flex flex-col items-center justify-center">
+               <Button submit="Cadastrar" />
+               <LinkLogin text="Login" href="/login" />
+            </div>
+         </form>
+      </section>
+   )
 }

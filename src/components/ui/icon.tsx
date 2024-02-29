679e2686479839b1805cab4c536499a03d860433
Matheus Ribeiro <srdmatheus@gmail.com>
2024-02-29 10:46:04 -0300

    chore: add chevron icon

---
+++ src/components/ui/icon.tsx
@@ -0,0 +1,11 @@
+import {
+    ChevronLeftIcon,
+    EyeIcon,
+    EyeSlashIcon
+} from '@heroicons/react/24/outline'
+
+export const Icon = {
+    eye: EyeIcon,
+    eyeSlash: EyeSlashIcon,
+    chevronLeft: ChevronLeftIcon
+}

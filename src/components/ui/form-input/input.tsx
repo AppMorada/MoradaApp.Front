8b6b28d88fc21ec464714bd11eff4596d1f68b30
Matheus Ribeiro <srdmatheus@gmail.com>
2024-03-01 10:38:09 -0300

    fix: remove unused class

--- src/components/ui/form-input/input.tsx
+++ src/components/ui/form-input/input.tsx
@@ -5,19 +5,19 @@ import { cn } from '@/lib/utils'
 type FormInputProps = ComponentProps<'input'>
 
 export const InputForm = forwardRef<HTMLInputElement, FormInputProps>(
-    ({ className, type, ...props }, ref) => {
-        return (
-            <input
-                className={cn(
-                    'font-nevermind bg-transparent font-medium outline-none placeholder:text-neutral',
-                    className
-                )}
-                {...props}
-                type={type}
-                ref={ref}
-            />
-        )
-    }
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          'bg-transparent font-medium outline-none placeholder:text-neutral',
+          className
+        )}
+        {...props}
+        type={type}
+        ref={ref}
+      />
+    )
+  }
 )
 
 InputForm.displayName = 'InputForm'

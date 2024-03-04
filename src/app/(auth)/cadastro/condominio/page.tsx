8c89926a9d6ddd7f446e4a7b4a80e7a04a6682f2
mfjack <ferreira.marlon@live.com>
2024-03-04 13:03:49 -0300

    feat: add button voltar nas páginas de registro

--- src/app/(auth)/cadastro/condominio/page.tsx
+++ src/app/(auth)/cadastro/condominio/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 
+import { ChevronLeftIcon } from '@heroicons/react/16/solid'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import z from 'zod'
@@ -62,76 +64,86 @@ export default function CondominiumRegisterPage() {
   }
 
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center">
-      <Logo />
+    <>
+      <Link href={'/login'}>
+        <Button className=" bg-white px-8 py-8 text-start text-black">
+          <div className="flex h-14 w-32 items-center justify-center gap-2 rounded-full border border-[#E6E0FF]">
+            <ChevronLeftIcon className="h-8 w-8 rounded-full bg-primary text-white" />
+            Voltar
+          </div>
+        </Button>
+      </Link>
+      <section className="flex min-h-screen flex-col items-center justify-center">
+        <Logo />
 
-      <form
-        className="mt-12 w-full px-4 sm:w-[444px]"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="flex flex-col items-center justify-center gap-4">
-          <Input
-            label="Nome do usuário administrador"
-            type="text"
-            placeholder="Insira o nome do usuário"
-            {...register('fullName')}
-            error={errors.fullName?.message}
-          />
-          <Input
-            label="CNPJ"
-            type="text"
-            placeholder="XXX.XXX.XXX/0001-XX"
-            {...register('cnpj')}
-            error={errors.cnpj?.message}
-          />
-          <Input
-            label="E-mail"
-            type="email"
-            placeholder="Insira seu e-mail"
-            {...register('email')}
-            error={errors.email?.message}
-          />
-          <PasswordInput
-            label="Senha"
-            placeholder="Insira sua senha"
-            {...register('password')}
-            error={errors.password?.message}
-          />
-          <PasswordInput
-            label="Cnfirmar Senha"
-            placeholder="Confirme sua senha"
-            {...register('password')}
-            error={errors.password?.message}
-          />
-          <Input
-            label="Nome do Condomínio"
-            type="text"
-            placeholder="Insira o nome do condomínio"
-            {...register('condominium')}
-            error={errors.condominium?.message}
-          />
-          <Input
-            label="Endereço"
-            type="text"
-            placeholder="Insira o endereço do condomínio"
-            {...register('address')}
-            error={errors.address?.message}
-          />
-        </div>
-        <div className="mt-8 flex flex-col items-center justify-center">
-          <Button>Confirmar Cadastro</Button>
-          <LinkLogin href="/login" text="Login" />
-        </div>
-      </form>
+        <form
+          className="mt-12 w-full px-4 sm:w-[444px]"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col items-center justify-center gap-4">
+            <Input
+              label="Nome do usuário administrador"
+              type="text"
+              placeholder="Insira o nome do usuário"
+              {...register('fullName')}
+              error={errors.fullName?.message}
+            />
+            <Input
+              label="CNPJ"
+              type="text"
+              placeholder="XXX.XXX.XXX/0001-XX"
+              {...register('cnpj')}
+              error={errors.cnpj?.message}
+            />
+            <Input
+              label="E-mail"
+              type="email"
+              placeholder="Insira seu e-mail"
+              {...register('email')}
+              error={errors.email?.message}
+            />
+            <PasswordInput
+              label="Senha"
+              placeholder="Insira sua senha"
+              {...register('password')}
+              error={errors.password?.message}
+            />
+            <PasswordInput
+              label="Cnfirmar Senha"
+              placeholder="Confirme sua senha"
+              {...register('password')}
+              error={errors.password?.message}
+            />
+            <Input
+              label="Nome do Condomínio"
+              type="text"
+              placeholder="Insira o nome do condomínio"
+              {...register('condominium')}
+              error={errors.condominium?.message}
+            />
+            <Input
+              label="Endereço"
+              type="text"
+              placeholder="Insira o endereço do condomínio"
+              {...register('address')}
+              error={errors.address?.message}
+            />
+          </div>
+          <div className="mt-8 flex flex-col items-center justify-center">
+            <Button>Confirmar Cadastro</Button>
+            <LinkLogin href="/login" text="Login" />
+          </div>
+        </form>
 
-      {isModalOpen && (
-        <Modal
-          onClose={onCloseModal}
-          title="Verifique seu e-mail"
-          paragraph="A administração do seu condomínio está a um passo de se tornar mais eficaz.
+        {isModalOpen && (
+          <Modal
+            onClose={onCloseModal}
+            title="Verifique seu e-mail"
+            paragraph="A administração do seu condomínio está a um passo de se tornar mais eficaz.
           Clique no botão abaixo para verificar seu e-mail e confirmar seu cadastro."
-        />
-      )}
-    </section>
+          />
+        )}
+      </section>
+    </>
   )
 }
--- src/app/(auth)/cadastro/morador/page.tsx
+++ src/app/(auth)/cadastro/morador/page.tsx
@@ -1,7 +1,9 @@
 'use client'
 
 import { useState } from 'react'
+import Link from 'next/link'
 
+import { ChevronLeftIcon } from '@heroicons/react/16/solid'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
@@ -61,61 +63,71 @@ export default function ResidentRegisterPage() {
   }
 
   return (
-    <section className="flex min-h-screen flex-col items-center justify-center">
-      <Logo />
-      <form
-        className="mt-12 w-full px-4 sm:w-[444px]"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <div className="flex flex-col items-center justify-center gap-4">
-          <Input
-            label=" Nome Completo"
-            type="text"
-            placeholder="Insira o nome do usuário"
-            {...register('fullName')}
-            error={errors.fullName?.message}
-          />
-          <Input
-            label=" CPF"
-            type="text"
-            placeholder="000.000.000-00"
-            {...register('cpf')}
-            error={errors.cpf?.message}
-          />
-          <Input
-            label="E-mail"
-            type="email"
-            placeholder="Insira seu e-mail"
-            {...register('email')}
-            error={errors.email?.message}
-          />
-          <PasswordInput
-            label="Senha"
-            placeholder="Insira sua senha"
-            {...register('password')}
-            error={errors.password?.message}
-          />
-          <PasswordInput
-            label="Cnfirmar Senha"
-            placeholder="Confirme sua senha"
-            {...register('password')}
-            error={errors.password?.message}
-          />
-        </div>
-        <div className="mt-8 flex flex-col items-center justify-center">
-          <Button>Confirmar Cadastro</Button>
-          <LinkLogin href="/login" text="Login" />
-        </div>
-      </form>
+    <>
+      <Link href={'/login'}>
+        <Button className=" bg-white px-8 py-8 text-start text-black">
+          <div className="flex h-14 w-32 items-center justify-center gap-2 rounded-full border border-[#E6E0FF]">
+            <ChevronLeftIcon className="h-8 w-8 rounded-full bg-primary text-white" />
+            Voltar
+          </div>
+        </Button>
+      </Link>
+      <section className="flex min-h-screen flex-col items-center justify-center">
+        <Logo />
+        <form
+          className="mt-12 w-full px-4 sm:w-[444px]"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex flex-col items-center justify-center gap-4">
+            <Input
+              label=" Nome Completo"
+              type="text"
+              placeholder="Insira o nome do usuário"
+              {...register('fullName')}
+              error={errors.fullName?.message}
+            />
+            <Input
+              label=" CPF"
+              type="text"
+              placeholder="000.000.000-00"
+              {...register('cpf')}
+              error={errors.cpf?.message}
+            />
+            <Input
+              label="E-mail"
+              type="email"
+              placeholder="Insira seu e-mail"
+              {...register('email')}
+              error={errors.email?.message}
+            />
+            <PasswordInput
+              label="Senha"
+              placeholder="Insira sua senha"
+              {...register('password')}
+              error={errors.password?.message}
+            />
+            <PasswordInput
+              label="Cnfirmar Senha"
+              placeholder="Confirme sua senha"
+              {...register('password')}
+              error={errors.password?.message}
+            />
+          </div>
+          <div className="mt-8 flex flex-col items-center justify-center">
+            <Button>Confirmar Cadastro</Button>
+            <LinkLogin href="/login" text="Login" />
+          </div>
+        </form>
 
-      {isModalOpen && (
-        <Modal
-          onClose={onCloseModal}
-          title="Cadastrado realizado com sucesso!"
-          paragraph="Faça login no nosso aplicativo para ter acesso 
+        {isModalOpen && (
+          <Modal
+            onClose={onCloseModal}
+            title="Cadastrado realizado com sucesso!"
+            paragraph="Faça login no nosso aplicativo para ter acesso 
           completo aos nossos serviços "
-        />
-      )}
-    </section>
+          />
+        )}
+      </section>
+    </>
   )
 }

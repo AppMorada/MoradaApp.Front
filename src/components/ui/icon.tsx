1b5b21df3c474547a7e53422b0b6ea488d4e0a4b
Matheus Ribeiro <srdmatheus@gmail.com>
2024-03-17 15:42:57 -0300

    feat(component): add icon component

---
+++ src/components/ui/icon.tsx
@@ -0,0 +1,5 @@
+import { ChevronLeftIcon } from '@heroicons/react/16/solid'
+
+export const Icon = {
+  chevronLeft: ChevronLeftIcon
+}
---
+++ src/components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from './button'
+export * from './input'
+export * from './linkLogin'
+export * from './logo'
+export * from './modal'
+export * from './passwordInput'
+export * from './icon'
